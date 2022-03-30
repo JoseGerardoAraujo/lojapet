11c0525ec1614db67f285c7e7c82fc786e48b79b
JoseGerardoAraujo <101367640+JoseGerardoAraujo@users.noreply.github.com>
2022-03-29 21:12:51 -0300

    commit inicial
    primeiro commit

---
+++ appCadastro/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from appCadastro.models import Endereco, Cliente, Animal, Servico
+
+admin.site.register(Endereco)
+admin.site.register(Cliente)
+admin.site.register(Animal)
+admin.site.register(Servico)
+
+
